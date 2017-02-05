135031a2092cd7e8092b1d89e8600e7e3ad69291
gagarin55 <gagarin55@protonmail.com>
2017-02-05 18:19:25 +0300

    some exports added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export * from './utils/base58';
+export * from './blockchain/network-parameters';
+export * from './blockchain/account/account';
+export * from './blockchain/account/address';
 
 export default class Waves {
     constructor() {
