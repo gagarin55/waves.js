a4f35a7200be21a6a9ca3e4176d60d23b4ab4cd0
gagarin55 <gagarin55@protonmail.com>
2017-03-09 17:19:09 +0300

    webpack target = node

--- webpack.config.js
+++ webpack.config.js
@@ -6,37 +6,47 @@ var libraryName = 'Waves';
 
 var plugins = [], outputFile;
 
+plugins.push(new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+}));
+
 if (env === 'build') {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
-    outputFile = libraryName.toLowerCase() + '.min.js';
+  plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+  outputFile = libraryName.toLowerCase() + '.min.js';
 } else {
-    outputFile = libraryName.toLowerCase() + '.js';
+  outputFile = libraryName.toLowerCase() + '.js';
 }
 
 var config = {
-    entry: __dirname + '/src/index.js',
-    devtool: 'source-map',
-    output: {
-        path: __dirname + '/dist',
-        filename: outputFile,
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        loaders: [
-            {
-                test: /(\.jsx|\.js)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        root: path.resolve('./src'),
-        extensions: ['', '.js']
-    },
-    plugins: plugins
+  target: 'node',
+  node: { process: false },
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
 };
 
 module.exports = config;
